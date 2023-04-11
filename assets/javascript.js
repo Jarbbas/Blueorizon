b86fdc826cad9eee6abc3d112c0da4f96bf0bb1c
Emanuel Rodrigues <44001420+Jarbbas@users.noreply.github.com>
2023-04-11 14:42:03 +0100

    terceiro commit
    overrall da página está bom, ainda por validar em modo mobile

--- assets/javascript.js
+++ assets/javascript.js
@@ -5,3 +5,16 @@ menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active');
   showcase.classList.toggle('active');
 })
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: 'left',
+  distance: '60px',
+  duration: 2000,
+  delay: 200,
+});
+
+sr.reveal('.about, .home, .projects',{delay: 100}); 
+sr.reveal('.about__img, .project__img, .map__img',{delay: 500}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
